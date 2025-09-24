3d9d1c0218fefbdd055523dc73de5faefa648a03
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2025-09-24 10:00:33 -0400

    movie card component created

---
+++ movie-search-engine/src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+
+function MovieCard({movie}) {
+
+    function onFavoriteClick() {
+        alert("Favorited")
+    }
+
+    return (
+        <>
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>🤍</button>
+                </div>
+            </div>
+
+            <div className="info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default MovieCard
